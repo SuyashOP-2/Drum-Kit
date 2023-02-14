be3334765019fcb05f3a772e4437c2208df6446d
Suyash <=>
2023-02-14 22:10:03 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+
+for(var i=0; i<document.querySelectorAll(".drum").length;){
+
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+        alert("I got clicked!");
+    
+    
+    });
+    
+
+
+
+}
+
+
